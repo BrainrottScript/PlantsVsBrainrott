c20a167d1afc1cb57fb0e9f296218dd13eed6fb7
BrainrottScript <onlydevs86@gmail.com>
2025-09-30 21:19:58 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BrainrottScript/PlantsVsBrainrott/refs/heads/main/lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
